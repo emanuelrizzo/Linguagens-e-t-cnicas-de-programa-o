b2f1b33f64dd53229c1c4bd512b3c8e49915391b
emanuelrizzo <emanuelrizzo26@gmail.com>
2026-08-04 09:29:43 -0300

    Add files via upload

---
+++ Ex - Triangulo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+int base, altura; 
+float area;
+
+base = 10;
+altura = 20;
+
+area = (base*altura)/2;
+
+printf ("A area do Triangulo Retangulo de Base = %d e Altura = %d e %f",base,altura,area);
+
+	return 0;
+}
